d671c19ebd9b817f22cdc8076cb8487ebe94a6ca
Snooker4real <cindanojonathan@icloud.com>
2022-04-05 20:26:30 +0200

    Create Post form done

---
+++ components/Post.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Post = () => {
+    return (<div className="flex flex-col">
+                <div className="bg-white mt-6 rounded-md p-4">
+                    <div className="flex items-center">
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/800px-2021_Facebook_icon.svg.png"
+                            className="rounded-full w-10 h-10"/>
+                    </div>
+                </div>
+            </div>
+    );
+};
+
+export default Post;
