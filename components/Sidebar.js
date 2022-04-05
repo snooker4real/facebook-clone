146639afbf1b36cbc5b5c6636e9baf11e79564e7
Snooker4real <cindanojonathan@icloud.com>
2022-04-05 19:33:49 +0200

    Feed started

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -9,18 +9,20 @@ import {
 } from "react-icons/md";
 import {AiOutlineShop} from "react-icons/ai";
 import {BsStopwatch} from "react-icons/bs";
+import {useSession} from "next-auth/react";
 
 
 const Sidebar = () => {
+    const {data : session} = useSession();
     return (
         <div className="hidden lg:inline-flex flex-col py-2 pl-2 max-w-xl lg:min-w-[302px]">
             <div className="flex items-center space-x-2 py-3 pl-4 hover:bg-gray-200 rounded-l-xl cursor-pointer">
                 <Image
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/800px-2021_Facebook_icon.svg.png"
+                    src={session?.user.image}
                     height={40}
                     width={40}
                     className="rounded-full cursor-pointer"/>
-                <p className="hidden sm:inline-flex font-medium">Cindano Jonathan</p>
+                <p className="hidden sm:inline-flex font-medium">{session?.user.name}</p>
             </div>
             <SidebarItem Icon={ImUsers} value="Friends"/>
             <SidebarItem Icon={MdGroups} value="Groups"/>
